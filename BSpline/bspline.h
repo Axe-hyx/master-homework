bf682e1a0ede5b27132ccd5bc6d05af479484333
woofhyx <hyx657362664@mail.dlut.edu.cn>
2019-12-10 17:20:58 +0800

    commit data file

--- BSpline/bspline.h
+++ BSpline/bspline.h
@@ -5,11 +5,25 @@
 using namespace std;
 using namespace Eigen;
 
-vector<Vector3d> points;
-vector<float> param_t;
-vector <float> knot;
-int n, degree, order;
-MatrixXd P, D;
+struct Curve {
+	vector<float> param_t;
+	vector <float> knot;
+	int n, degree, order;
+	vector<Vector3d> points;
+	MatrixXd P, D;
+}curve;
+bool debug = 1;
+
+struct {
+	vector<float> param_s, param_t;
+	vector <float> knot_U, knot_V;
+	vector<vector<Vector3d>> points;
+	vector<MatrixXd> P, D;
+	int n, m;
+	int p, q; // degree
+	int order_p, order_q; // order
+}surface;
+
 enum en_t {
 	UNIFORM_SPACED,
 	CHORD_LENGTH
@@ -20,12 +34,11 @@ enum en_knot {
 	UNIVER
 };
 
-bool debug = 1;
 
-const double Nikt(int i, int k, double t) 
+const double Nikt(int i, int k, double t)
 {
 	using namespace std;
-	auto& x = knot;
+	auto& x = curve.knot;
 	if (k == 1)
 	{
 		if (t >= x[i] && t < x[i + 1]) return 1;
@@ -44,9 +57,9 @@ const double Nikt(int i, int k, double t)
 	return a + b;
 }
 
-float cal_N(int i, int k, float t) {
-	if (k ==1) {
-		if (t >= knot[i] && t < knot[i + 1])
+float cal_N(Curve& cv, int i, int k, float t) {
+	if (k == 1) {
+		if (t >= cv.knot[i] && t < cv.knot[i + 1])
 		{
 			return 1.0;
 		}
@@ -55,23 +68,23 @@ float cal_N(int i, int k, float t) {
 			return 0.0;
 		}
 	}
-	float lfrac = (knot[i + k - 1] - knot[i]);
-	float rfrac = (knot[i + k] - knot[i + 1]);
+	float lfrac = (cv.knot[i + k - 1] - cv.knot[i]);
+	float rfrac = (cv.knot[i + k] - cv.knot[i + 1]);
 	float a = 0.0f, b = 0.0f;
 	if (abs(lfrac) < 1e-6 && abs(rfrac) < 1e-6) {
 		return 0.0;
 	}
-	else if (abs(lfrac ) < 1e-6) {
-		a = (knot[i + k] - t) / rfrac * cal_N(i + 1, k - 1, t);
+	else if (abs(lfrac) < 1e-6) {
+		a = (cv.knot[i + k] - t) / rfrac * cal_N(cv, i + 1, k - 1, t);
 	}
-	else if (abs(rfrac) < 1e-6 ) {
-		b = (t - knot[i]) / lfrac * cal_N(i, k - 1, t);
+	else if (abs(rfrac) < 1e-6) {
+		b = (t - cv.knot[i]) / lfrac * cal_N(cv, i, k - 1, t);
 	}
 	else {
-		a = (knot[i + k] - t) / rfrac * cal_N(i + 1, k - 1, t);
-		b = (t - knot[i]) / lfrac * cal_N(i, k - 1, t);
+		a = (cv.knot[i + k] - t) / rfrac * cal_N(cv, i + 1, k - 1, t);
+		b = (t - cv.knot[i]) / lfrac * cal_N(cv, i, k - 1, t);
 	}
-	if (i == 0 && k == 3 && t == 0.01f) debug = 0; 
+	if (i == 0 && k == 3 && t == 0.01f) debug = 0;
 	return a + b;
 }
 
@@ -81,7 +94,7 @@ double distance(Vector3d& v1, Vector3d& v2) {
 /*
 get parm t
 */
-void generatePram(en_t type, int a, int b) {
+void generatePram(vector<Vector3d> &points, vector<float> &param_t, int n, en_t type, int a, int b) {
 	param_t.clear();
 	double length = 0.0;
 	double ct = 0;
@@ -102,7 +115,7 @@ void generatePram(en_t type, int a, int b) {
 				ct += distance(points[i], points[i - 1]);
 				param_t.push_back((float)ct / length);
 			}
-			
+
 		}
 		break;
 	default:
@@ -111,54 +124,52 @@ void generatePram(en_t type, int a, int b) {
 	if (debug) {
 		cout << "T :";
 		for (int i = 0; i < param_t.size(); ++i) {
-			cout <<  param_t[i] << " ";
+			cout << param_t[i] << " ";
 		}
 		cout << endl;
 	}
-	
-
 }
 
 /*
 	generate knot
 */
-void generateKnot(en_knot type) {
-	knot.clear();
+void generateKnot(Curve& cv, en_knot type) {
+	cv.knot.clear();
 	if (type == UNIVER) {
-		for (int i = 1; i <= n+1+order;++i) {
-			knot.push_back((float)i / (n+order+1));
+		for (int i = 1; i <= cv.n + 1 + cv.order;++i) {
+			cv.knot.push_back((float)i / (cv.n + cv.order + 1));
 		}
 	}
 	else if (type == k_SPACED) {
-		for (int i = 0; i <order;++i) {
-			knot.push_back(0);
+		for (int i = 0; i < cv.order;++i) {
+			cv.knot.push_back(0);
 		}
-		for (int i = 1;i <= n - degree;++i) {
-			knot.push_back((float)i / (n - degree + 1));
+		for (int i = 1;i <= cv.n - cv.degree;++i) {
+			cv.knot.push_back((float)i / (cv.n - cv.degree + 1));
 		}
-		for (int i = 0; i < order;++i) {
-			knot.push_back(1);
+		for (int i = 0; i < cv.order;++i) {
+			cv.knot.push_back(1);
 		}
 	}
 	else if (type == AVERAGE) {
-		for (int i = 0; i < order;++i) {
-			knot.push_back(0);
+		for (int i = 0; i < cv.order;++i) {
+			cv.knot.push_back(0);
 		}
-		for (int j = 1; j <= n - degree;++j) {
+		for (int j = 1; j <= cv.n - cv.degree;++j) {
 			double avg = 0.0;
-			for (int i = j; i <= j + degree - 1;++i) {
-				avg += param_t[i];
+			for (int i = j; i <= j + cv.degree - 1;++i) {
+				avg += cv.param_t[i];
 			}
-			knot.push_back(avg / degree);
+			cv.knot.push_back(avg / cv.degree);
 		}
-		for (int i = 0; i < order;++i) {
-			knot.push_back(1);
+		for (int i = 0; i < cv.order;++i) {
+			cv.knot.push_back(1);
 		}
 	}
-	if(debug){
-		cout << "KNOT: " << knot.size();
-		for (int i = 0; i < knot.size(); ++i) {
-			cout << knot[i] << " ";
+	if (debug) {
+		cout << "KNOT: " << cv.knot.size();
+		for (int i = 0; i < cv.knot.size(); ++i) {
+			cout << cv.knot[i] << " ";
 		}
 		cout << endl;
 	}
@@ -168,58 +179,72 @@ void drawPoly(int i, int k, float t) {
 	float step = 0.1f;
 	fprintf(stderr, "N(%d,%d): ", i, k);
 	for (float c = 0; c < 1;c += step) {
-		fprintf(stderr, "%f ", cal_N(i, k, c));
+		fprintf(stderr, "%f ", cal_N(curve, i, k, c));
 	}
-	fprintf(stderr, "%f ", cal_N(i, k, t));
+	fprintf(stderr, "%f ", cal_N(curve, i, k, t));
 	fprintf(stderr, "\n");
 }
-void evaluate() {
-	MatrixXd N(n + 1, n + 1);
-	for (int i = 0; i <= n; ++i) {
-		for (int j = 0; j <= n; ++j) {
-			N(i, j) = Nikt(j, order, param_t[i]);
+void evaluate(Curve& cv) {
+	MatrixXd N(cv.n + 1, cv.n + 1);
+	for (int i = 0; i <= cv.n; ++i) {
+		for (int j = 0; j <= cv.n; ++j) {
+			N(i, j) = cal_N(cv, j, cv.order, cv.param_t[i]);
 		}
 	}
-	N(n, n) = 1;
+	N(cv.n, cv.n) = 1;
 	cout << "N: " << endl;
 	cout << N << endl;
-	D.resize(n + 1, 3);
-	for (int i = 0; i <= n; ++i) {
+	cv.D.resize(cv.n + 1, 3);
+	for (int i = 0; i <= cv.n; ++i) {
 		for (int j = 0; j < 3; ++j) {
-			D(i, j) = points[i](j);
+			cv.D(i, j) = cv.points[i](j);
 		}
 	}
 	cout << "D: " << endl;
-	cout << D << endl;
-	P.resize(n + 1, 3);
-	P =N.inverse() * D;
+	cout << cv.D << endl;
+	cv.P.resize(cv.n + 1, 3);
+	cv.P = N.inverse() * cv.D;
 	cout << "P: " << endl;
-	cout << P << endl;
+	cout << cv.P << endl;
 }
-void testcase() {
-	readdata("D:\\working\\B-spline\\BSpline\\Release\\data.in");
-	n = points.size() -1;
-	degree = 2;
-	order = degree + 1;
-	if (n < degree) {
+void global_curve_interpolation() {
+	readdata(curve.points, "D:\\working\\master-homework\\BSpline\\Release\\curve_data.in");
+	curve.n = curve.points.size() - 1;
+	curve.degree = 3;
+	curve.order = curve.degree + 1;
+	if (curve.n < curve.degree) {
 		fprintf(stderr, "need degree +1 control points\n");
 		exit(1);
 	}
-	generatePram(en_t::UNIFORM_SPACED, 0, 1);
-	cout << "n+1: "<<n + 1 << " degree "<<degree<<endl;
-	generateKnot(en_knot::k_SPACED);
-	evaluate();
+	generatePram(curve.points, curve.param_t, curve.n, en_t::UNIFORM_SPACED, 0, 1);
+	cout << "n+1: " << curve.n + 1 << " degree " << curve.degree << endl;
+	generateKnot(curve, en_knot::k_SPACED);
+	evaluate(curve);
 }
-void drawBspline() {
+
+void globel_surface_interpolation() {
+	readSurdata(surface.points, "D:\\working\\master-homework\\BSpline\\Release\\surface_data.in",
+		surface.m, surface.n);
+	surface.p = 2;
+	surface.q = 2;
+	if (surface.m < surface.p || surface.n < surface.q) {
+		fprintf(stderr, "need degree +1 control points\n");
+		exit(1);
+	}
+}
+void testcase() {
+	global_curve_interpolation();
+}
+void drawBspline(Curve& cv) {
 	glPointSize(4);
 	glColor3f(0, 0, 1);
 	glBegin(GL_POINTS);
 	float step = 0.01f;
-	
+
 	for (float t = 0.0; t <= 1.0; t += step) {
 		Vector3d v(0, 0, 0);
-		for (int i = 0; i < n+1; ++i) {
-			v += Vector3d(P(i,0), P(i,1), P(i,2)) * cal_N(i, order, t);
+		for (int i = 0; i < cv.n + 1; ++i) {
+			v += Vector3d(cv.P(i, 0), cv.P(i, 1), cv.P(i, 2)) * cal_N(cv, i, cv.order, t);
 		}
 		glVertex3f(v(0), v(1), v(2));
 	}
@@ -228,20 +253,21 @@ void drawBspline() {
 	glPointSize(10);
 	glColor3f(1, 0, 0);
 	glBegin(GL_POINTS);
-	for (float t = 0; t <= n; t++) {
-		glVertex3f(D(t, 0), D(t, 1), D(t, 2));
+	for (float t = 0; t <= cv.n; t++) {
+		glVertex3f(cv.D(t, 0), cv.D(t, 1), cv.D(t, 2));
 	}
 	glEnd();
 	glPointSize(10);
 	glColor3f(0, 1, 0);
 	glBegin(GL_POINTS);
-	for (float t = 0; t <= n; t++) {
-		glVertex3f(P(t, 0), P(t, 1), P(t, 2));
+	for (float t = 0; t <= cv.n; t++) {
+		glVertex3f(cv.P(t, 0), cv.P(t, 1), cv.P(t, 2));
 	}
 	glEnd();
 }
 
 void displayB() {
+	auto cv = curve;
 	glColor3f(1, 1, 1);
 	glBegin(GL_LINES);
 	glVertex3d(0, 0, 0);
@@ -254,11 +280,11 @@ void displayB() {
 
 	glColor3f(1, 0, 1);
 	glBegin(GL_LINES);
-	for (int i = 1; i < points.size(); ++i) {
-		glVertex3d(points[i - 1](0), points[i - 1](1), points[i - 1](2));
-		glVertex3d(points[i](0), points[i](1), points[i](2));
+	for (int i = 1; i < cv.points.size(); ++i) {
+		glVertex3d(cv.points[i - 1](0), cv.points[i - 1](1), cv.points[i - 1](2));
+		glVertex3d(cv.points[i](0), cv.points[i](1), cv.points[i](2));
 	}
 	glEnd();
-	drawBspline();
+	drawBspline(cv);
 	glFlush();
 }
--- BSpline/filehelper.h
+++ BSpline/filehelper.h
@@ -7,15 +7,39 @@
 #include <Eigen/Dense>
 using namespace std;
 using namespace Eigen;
-extern vector<Vector3d> points;
 
-void readdata(const char* path) {
+void readdata(vector<Vector3d> &points , const char* path) {
 	std::ifstream file(path);
 	string line;
+	points.clear();
 	while (getline(file, line)) {
 		istringstream iss(line);
 		double x, y, z, w;
 		iss >> x >> y >> z >> w;
 		points.push_back(Vector3d(x, y, z));
 	}
+}
+
+void readSurdata(vector<vector<Vector3d>>& points, const char* path, int &u , int &v) {
+	std::ifstream file(path);
+	string line;
+	points.clear();
+	getline(file, line);
+	istringstream iss(line);
+	iss >> u >> v;
+	for (int i = 0; i < u; ++i) {
+		for (int k = 0; k < v; ++k) {
+			if(!getline(file,line))
+			{
+				fprintf(stderr, "data not enough\n");
+				exit(1);
+			}
+			istringstream iss(line);
+			double x, y, z, w;
+			iss >> x >> y >> z >> w;
+			points[i].push_back(Vector3d(x, y, z));
+		}
+	}
+	u--;
+	v--;
 }
